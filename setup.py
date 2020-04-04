bc261328e975da05fbc67931a34b57909b00e800
martin <martin.sachin@gmail.com>
2020-04-04 16:41:25 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="rayvis",
+    version="0.0.1",
+    author="Martin Sach",
+    author_email="martin.sachin@gmail.com",
+    description="Tool to visualize various mesh files and more provided by raytracer.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/SachCZ/rayvis",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.0',
+    install_requires=[
+        "matplotlib",
+        "numpy",
+        "vtk"
+    ]
+)
